2ae0377e1f9f26eb5f2d9facfdc119b3409a6123
Khaled Alhendi <khaled.alhendi@its.ws>
2018-05-23 11:09:17 +0300

    garbage

--- javascript/buildScript/srcServer.js
+++ javascript/buildScript/srcServer.js
@@ -1,40 +1,37 @@
-import  express  from 'express'; 
-import path from 'path';
-import open from 'open';
-import webpack from 'webpack';
-import config from '../webpack.config.dev';
+//@ts-check
 
-const port = 3000; 
-const app = express(); 
+import express from "express";
+import path from "path";
+import open from "open";
+import webpack from "webpack";
+import config from "../webpack.config.dev";
+
+const port = 3000;
+const app = express();
 const compiler = webpack(config);
 
-app.use(require('webpack-dev-middleware')(compiler, {
-	noInfo: true, 
-	publicPath: config.output.publicPath, 
-}))
+app.use(
+	require("webpack-dev-middleware")(compiler, {
+		publicPath: config.output.publicPath
+	})
+);
 
-app.get('/', function(req, res)
-{
-	res.sendfile(path.join(__dirname, '../src/index.html'));
+app.get("/", function(req, res) {
+	res.sendfile(path.join(__dirname, "../src/index.html"));
 });
 
-app.get('/users', function(req, res)
-{
+app.get("/users", function(req, res) {
 	res.json([
-		{"id":1,"firstName":"Bob"},
-		{"id":2,"firstName":"Tammy"},
-		{"id":3,"firstName":"Bingo"}
+		{ id: 1, firstName: "Bob" },
+		{ id: 2, firstName: "Tammy" },
+		{ id: 3, firstName: "Bingo" }
 	]);
 });
 
-app.listen(port, function(err)
-{
-	if(err)
-	{
-	console.log(err); 
+app.listen(port, function(err) {
+	if (err) {
+		console.log(err);
+	} else {
+		open("http://localhost:" + port);
 	}
-	else
-	{
-		open('http://localhost:'+ port); 
-	}
-}); 
+});
---
+++ test/presedentOrder.js
@@ -0,0 +1,6 @@
+let i = 0;
+
+console.log("my name is");
+for (let i = 0; i < 5; i++) {
+  console.log(i + "yo");
+}
